47247730c2d2cdea5b4e2f32a13859128a28d82b
4mede <4mede@tutamail.com>
2024-07-30 20:28:50 -0400

    Add files via upload

---
+++ README.md
@@ -0,0 +1,21 @@
+## qssi-user 10 QKQ1.200614.002 0370_202106232216 release-keys
+- Manufacturer: realme
+- Platform: bengal
+- Codename: RECE4244
+- Brand: realme
+- Flavor: qssi-user
+- Release Version: 10
+- Id: QKQ1.200614.002
+- Incremental: 0370_202106232216
+- Tags: release-keys
+- CPU Abilist: arm64-v8a,armeabi-v7a,armeabi
+- A/B Device: false
+- Locale: en-US
+- Screen Density: undefined
+- Fingerprint: 
+- OTA version: RMX2195PU_11.A.37_0370_202106232216
+- Branch: RMX2195PU_11.A.37_0370_202106232216
+- Repo: realme_rece4244_dump
+
+
+>Dumped by [Phoenix Firmware Dumper](https://github.com/DroidDumps/phoenix_firmware_dumper)
---
+++ join_split_files.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+cat boot.img.* 2>/dev/null >> boot.img
+rm -f boot.img.* 2>/dev/null
+cat bootimg/17_dtbdump_#p]u.dtb.* 2>/dev/null >> bootimg/17_dtbdump_#p]u.dtb
+rm -f bootimg/17_dtbdump_#p]u.dtb.* 2>/dev/null
+cat system/system/priv-app/Browser/Browser.apk.* 2>/dev/null >> system/system/priv-app/Browser/Browser.apk
+rm -f system/system/priv-app/Browser/Browser.apk.* 2>/dev/null
+cat system/system/priv-app/Settings/Settings.apk.* 2>/dev/null >> system/system/priv-app/Settings/Settings.apk
+rm -f system/system/priv-app/Settings/Settings.apk.* 2>/dev/null
+cat system/system/priv-app/OppoGallery2/OppoGallery2.apk.* 2>/dev/null >> system/system/priv-app/OppoGallery2/OppoGallery2.apk
+rm -f system/system/priv-app/OppoGallery2/OppoGallery2.apk.* 2>/dev/null
+cat system/system/reserve/Photos.apk.* 2>/dev/null >> system/system/reserve/Photos.apk
+rm -f system/system/reserve/Photos.apk.* 2>/dev/null
+cat system/system/preload/com.google.android.apps.photos-9/com.google.android.apps.photos-9.apk.* 2>/dev/null >> system/system/preload/com.google.android.apps.photos-9/com.google.android.apps.photos-9.apk
+rm -f system/system/preload/com.google.android.apps.photos-9/com.google.android.apps.photos-9.apk.* 2>/dev/null
+cat system/system/preload/HeyFun/HeyFun.apk.* 2>/dev/null >> system/system/preload/HeyFun/HeyFun.apk
+rm -f system/system/preload/HeyFun/HeyFun.apk.* 2>/dev/null
+cat system/system/preload/OppoVideoEditor/OppoVideoEditor.apk.* 2>/dev/null >> system/system/preload/OppoVideoEditor/OppoVideoEditor.apk
+rm -f system/system/preload/OppoVideoEditor/OppoVideoEditor.apk.* 2>/dev/null
+cat system/system/app/GoogleLatinInput/GoogleLatinInput.apk.* 2>/dev/null >> system/system/app/GoogleLatinInput/GoogleLatinInput.apk
+rm -f system/system/app/GoogleLatinInput/GoogleLatinInput.apk.* 2>/dev/null
+cat system/system/app/OppoCamera/OppoCamera.apk.* 2>/dev/null >> system/system/app/OppoCamera/OppoCamera.apk
+rm -f system/system/app/OppoCamera/OppoCamera.apk.* 2>/dev/null
+cat system/system/apex/com.android.runtime.release.apex.* 2>/dev/null >> system/system/apex/com.android.runtime.release.apex
+rm -f system/system/apex/com.android.runtime.release.apex.* 2>/dev/null
+cat oppo_product/app/Photos/Photos.apk.* 2>/dev/null >> oppo_product/app/Photos/Photos.apk
+rm -f oppo_product/app/Photos/Photos.apk.* 2>/dev/null
+cat product/priv-app/GmsCore/GmsCore.apk.* 2>/dev/null >> product/priv-app/GmsCore/GmsCore.apk
+rm -f product/priv-app/GmsCore/GmsCore.apk.* 2>/dev/null
+cat product/priv-app/Velvet/Velvet.apk.* 2>/dev/null >> product/priv-app/Velvet/Velvet.apk
+rm -f product/priv-app/Velvet/Velvet.apk.* 2>/dev/null
+cat product/app/YouTube/YouTube.apk.* 2>/dev/null >> product/app/YouTube/YouTube.apk
+rm -f product/app/YouTube/YouTube.apk.* 2>/dev/null
+cat product/app/LatinImeGoogle/LatinImeGoogle.apk.* 2>/dev/null >> product/app/LatinImeGoogle/LatinImeGoogle.apk
+rm -f product/app/LatinImeGoogle/LatinImeGoogle.apk.* 2>/dev/null
+cat product/app/WebViewGoogle/WebViewGoogle.apk.* 2>/dev/null >> product/app/WebViewGoogle/WebViewGoogle.apk
+rm -f product/app/WebViewGoogle/WebViewGoogle.apk.* 2>/dev/null
+cat product/app/Maps/Maps.apk.* 2>/dev/null >> product/app/Maps/Maps.apk
+rm -f product/app/Maps/Maps.apk.* 2>/dev/null
+cat product/app/Messages/Messages.apk.* 2>/dev/null >> product/app/Messages/Messages.apk
+rm -f product/app/Messages/Messages.apk.* 2>/dev/null
